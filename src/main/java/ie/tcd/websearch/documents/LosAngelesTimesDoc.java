17ae0a382bdc1b2035216e4b4504c22ef5ab2694
sidgvpta <siddharth.gvpta@gmail.com>
2018-03-15 17:04:44 +0000

    Added Los Angeles Times Doc & Doc Parser Classes

--- src/main/java/ie/tcd/websearch/documents/LosAngelesTimesDoc.java
+++ src/main/java/ie/tcd/websearch/documents/LosAngelesTimesDoc.java
@@ -4,25 +4,28 @@ import lombok.Data;
 import org.apache.lucene.document.*;
 
 @Data
-public class ForeignBroadcastDoc extends BaseDoc {
+public class LosAngelesTimesDoc extends BaseDoc {
 
   private String originalId;
-  private String author;
   private String date;
   private String headline;
   private String byline;
-  private String publication = "Foreign Broadcast Information Service";
+  private String graphicCaption;
+  private String section;
+  private String publication = "Los Angeles Times";
 
   @Override
   public Document convertToLuceneDoc() {
     Document doc = new Document();
     doc.add(new StringField("doc_number", this.getDocNo(), Field.Store.YES));
-    doc.add(new StringField("author", this.getAuthor(), Field.Store.YES));
+    doc.add(new StringField("originalId", this.getOriginalId(), Field.Store.YES));
     doc.add(new StringField("date", this.getDate(), Field.Store.YES));
     doc.add(new TextField("headline", this.getHeadline(), Field.Store.YES));
     doc.add(new TextField("byline", this.getByline(), Field.Store.YES));
     doc.add(new TextField("text", this.getText(), Field.Store.YES));
+    doc.add(new TextField("graphicCaption", this.getGraphicCaption(), Field.Store.YES));
     doc.add(new TextField("publication", this.getPublication(), Field.Store.YES));
+    doc.add(new TextField("meta", this.getSection(), Field.Store.YES));
     doc.add(new IntPoint("length", this.getLength()));
 
     return doc;
---
+++ src/main/java/ie/tcd/websearch/parsers/LosAngelesTimesDocsParser.java
@@ -0,0 +1,58 @@
+package ie.tcd.websearch.parsers;
+
+import ie.tcd.websearch.documents.ForeignBroadcastDoc;
+import ie.tcd.websearch.documents.LosAngelesTimesDoc;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LosAngelesTimesDocsParser extends BaseParser {
+  private static String DOCUMENT_ROOT_PATH = "docs/latimes/";
+  private List<LosAngelesTimesDoc> losAngelesTimesDocs = new ArrayList<>();
+
+  public LosAngelesTimesDocsParser() {
+    List<Path> files = this.getFiles(DOCUMENT_ROOT_PATH);
+    int count = 0;
+    for(Path file : files) {
+      try {
+        List<Element> docs = this.getDocElements(file);
+        count += docs.size();
+        for (Element doc : docs) {
+          LosAngelesTimesDoc laDoc = new LosAngelesTimesDoc();
+
+          laDoc.setDocNo(doc.getChildTextTrim("DOCNO"));
+          laDoc.setOriginalId(doc.getChildTextTrim("DOCID"));
+          laDoc.setDate(doc.getChildTextTrim("DATE"));
+          laDoc.setHeadline(doc.getChildTextTrim("HEADLINE"));
+          laDoc.setByline(doc.getChildTextTrim("BYLINE"));
+          laDoc.setText(doc.getChildTextTrim("TEXT"));
+          laDoc.setGraphicCaption(doc.getChildTextTrim("GRAPHIC"));
+          laDoc.setSection(doc.getChildTextTrim("SECTION"));
+
+          if (laDoc.getText() != null) {
+              laDoc.setLength(laDoc.getText().length());
+          }
+
+//        this.losAngelesTimesDocs.add(laDoc);
+        }
+
+      } catch (IOException | JDOMException e) {
+          e.printStackTrace();
+        }
+    }
+
+    System.out.println(String.format("%s Processed %d docs", DOCUMENT_ROOT_PATH, count));
+  }
+
+  public List<LosAngelesTimesDoc> getDocs() {
+  return this.losAngelesTimesDocs;
+}
+
+  public void removeDocs() {
+  this.losAngelesTimesDocs.clear();
+}
+}
